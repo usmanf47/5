f731c4eaa342a7bcd87befe86e2a02a3b982e133
unknown <you@example.com>
2023-02-09 13:04:09 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,285 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-room",
+      "name": "Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.2555402984744539,
+        "pitch": 0.14840162830495096,
+        "fov": 1.2835367499726889
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1234370504114644,
+          "pitch": -0.5162511455200338,
+          "rotation": 7.0685834705770345,
+          "target": "4-stairs"
+        },
+        {
+          "yaw": 0.13429211507130745,
+          "pitch": 0.06671230410774598,
+          "rotation": 1.5707963267948966,
+          "target": "2-master-bedroom-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-open-bedroom",
+      "name": "Open Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.806635034757436,
+        "pitch": 0.4476832877977017,
+        "fov": 1.2835367499726889
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.32434705106393125,
+          "pitch": 0.10053578592895818,
+          "rotation": 0,
+          "target": "6-stairs"
+        },
+        {
+          "yaw": 0.9235802225388561,
+          "pitch": 0.061945646123861664,
+          "rotation": 1.5707963267948966,
+          "target": "3-bedroom-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-master-bedroom-",
+      "name": "Master Bedroom ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.032091537859260555,
+        "pitch": 0.11995670560491867,
+        "fov": 1.2835367499726889
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.623831339566216,
+          "pitch": 0.10529400386905863,
+          "rotation": 0,
+          "target": "5-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bedroom-",
+      "name": "Bedroom ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.296389359264495,
+        "pitch": 0.07008967642528674,
+        "fov": 1.2835367499726889
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.520786154644627,
+          "pitch": 0.04324580639067932,
+          "rotation": 0,
+          "target": "6-stairs"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-stairs",
+      "name": "Stairs",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.2786896708829705,
+        "pitch": 0.08876796214764227,
+        "fov": 1.2835367499726889
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.011360193381658945,
+          "pitch": 0.026242093762391505,
+          "rotation": 0,
+          "target": "1-open-bedroom"
+        },
+        {
+          "yaw": -0.9575275128996363,
+          "pitch": 0.027820359151931484,
+          "rotation": 0,
+          "target": "3-bedroom-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-living-room",
+      "name": "Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 1.4199586617237507,
+        "pitch": 0.11786104125829411,
+        "fov": 1.2835367499726889
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1238649598354602,
+          "pitch": 0.04247369055295991,
+          "rotation": 1.5707963267948966,
+          "target": "2-master-bedroom-"
+        },
+        {
+          "yaw": -1.0925681813546433,
+          "pitch": -0.5402402462908142,
+          "rotation": 0.7853981633974483,
+          "target": "4-stairs"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-stairs",
+      "name": "Stairs",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 1.4137556534251647,
+        "pitch": 0.8959057796378858,
+        "fov": 1.2835367499726889
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7061882942690119,
+          "pitch": 0.6384709055900277,
+          "rotation": 5.497787143782138,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": -0.9763642099323953,
+          "pitch": 0.024197334761300482,
+          "rotation": 0,
+          "target": "3-bedroom-"
+        },
+        {
+          "yaw": -0.009578681670205924,
+          "pitch": 0.10099591634643446,
+          "rotation": 0,
+          "target": "1-open-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "3Bhk Villa",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
